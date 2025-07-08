3a80ee89d18c13aa932fdc6986d0e99816c53328
C-Wolf206 <wolfc1921@gmail.com>
2025-07-07 23:44:54 -0500

    fixed location err

--- obj/Debug/netstandard2.1/WolfsCards.AssemblyInfo.cs
+++ obj/Debug/netstandard2.1/WolfsCards.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WolfsCards")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85532a252b3d39c5af6ff7862f1212c56ffb14f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4c6a093d0ffce7065980b89e98bf0b3f149ff10")]
 [assembly: System.Reflection.AssemblyProductAttribute("WolfsCards")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WolfsCards")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
